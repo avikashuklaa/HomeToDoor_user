bfbbfe3a4e2c63c3855c09391881068af038ddb3
Avika Shukla <80634978+avikashuklaa@users.noreply.github.com>
2023-05-08 23:52:32 +0530

    Add files via upload

---
+++ lib/mainScreens/cart_screen.dart
@@ -0,0 +1,232 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:hometodoor_user/mainScreens/address_screen.dart';
+import 'package:hometodoor_user/widgets/app_bar.dart';
+import 'package:hometodoor_user/widgets/cart_item_design.dart';
+import 'package:provider/provider.dart';
+
+import '../assistantMethods/assistant_methods.dart';
+import '../assistantMethods/cart_item_counter.dart';
+import '../assistantMethods/total_amount.dart';
+import '../models/items.dart';
+import '../splashScreen/splash_screen.dart';
+import '../widgets/progress_bar.dart';
+import '../widgets/text_widget_header.dart';
+
+class CartScreen extends StatefulWidget {
+
+  final String? chefUID;
+
+  CartScreen({this.chefUID});
+
+  @override
+  State<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  List<int>? seperateItemQuantityList;
+
+  num totalAmount = 0;
+
+  @override
+  void initState() {
+   super.initState();
+
+   totalAmount=0;
+   Provider.of<TotalAmount>(context, listen: false).displayTotalAmount(0);
+   seperateItemQuantityList = seperateItemQuantities();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xff2ec4b6),
+                Color(0xff023e8a),
+              ],
+              begin: const FractionalOffset(0.0, 0.0),
+              end: const FractionalOffset(1.0, 0.0),
+              stops: [0.0, 1.0],
+              tileMode: TileMode.clamp,
+            ),
+          ),
+        ),
+        leading: IconButton(
+          icon: Icon(
+              Icons.clear_all
+          ),
+          onPressed: (){
+              clearCartNow(context);
+          },
+        ),
+        //title: Text(
+        //    "HomeToDoor"
+       // ),
+        centerTitle: true,
+        automaticallyImplyLeading: true,
+        actions: [
+          Stack(
+            children: [
+              IconButton(
+                  onPressed: (){
+                    print("Clicked");
+                  },
+                  icon: Icon(Icons.shopping_cart)),
+              Positioned(
+                child: Stack(
+                  children: [
+                    Icon(
+                      Icons.brightness_1,
+                      size: 20.0,
+                      color: Colors.black,
+                    ),
+                    Positioned(
+                      top: 3,
+                      right: 4,
+                      child: Center(
+                        child: Consumer<CartItemCounter>(
+                          builder: (context, counter, c){
+                            return Text(counter.count.toString(),
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 12.0
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    )
+                  ],
+                ),),
+            ],
+          )
+        ],
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          SizedBox(width: 5,),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: FloatingActionButton.extended(
+              heroTag: "btn1",
+              label: Text(
+                "Clear cart",
+                style: TextStyle(fontSize: 15),
+              ),
+              backgroundColor: Color(0xff087e8b),
+              icon: Icon(
+                Icons.delete
+              ),
+              onPressed: (){
+                  clearCartNow(context);
+                  Navigator.push(context, MaterialPageRoute(builder: (c) => MySplashScreen()));
+                  Fluttertoast.showToast(msg: "The cart has been cleared.");
+              },
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: FloatingActionButton.extended(
+              heroTag: "btn2",
+              label: Text(
+                "Checkout",
+                style: TextStyle(fontSize: 15),
+              ),
+              backgroundColor: Color(0xff087e8b),
+              icon: Icon(
+                  Icons.navigate_next
+              ),
+              onPressed: (){
+                Navigator.push(context, MaterialPageRoute(
+                    builder: (c) =>
+                        AddressScreen(
+                          totalAmount: totalAmount.toDouble(),
+                          chefUID: widget.chefUID,
+                        )));
+
+              },
+            ),
+          )
+        ],
+      ),
+      body: CustomScrollView(
+        slivers: [
+          SliverPersistentHeader(
+              pinned: true,
+              delegate: TextWidgetHeader(title: "Cart")
+          ),
+
+          SliverToBoxAdapter(
+            child: Consumer2<TotalAmount, CartItemCounter>(builder: (context, amountProvider, cartProvider, c){
+              return Padding(
+                padding: EdgeInsets.all(8),
+                child: Center(
+                  child: cartProvider.count == 0
+                      ? Container()
+                  : Text("Total Amount: ${amountProvider.tAmount.toString()}",
+                  style: TextStyle(
+                    color: Color(0xff087e8b),
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w500
+                  ),),
+                ),
+              );
+            })
+          ),
+
+          StreamBuilder(
+              stream: FirebaseFirestore.instance.
+              collection("items").where("itemID", whereIn: seperateItemIDs())
+                  .orderBy("publishedDate", descending: true)
+            .snapshots(),
+            builder: (context, snapshot){
+                return !snapshot.hasData
+                    ? SliverToBoxAdapter(child: Center(child: circularProgress(),),)
+                    : snapshot.data!.docs.length == 0
+                    ? Container()
+                    : SliverList(
+                  delegate: SliverChildBuilderDelegate((context, index){
+                    Items model = Items.fromJson(snapshot.data!.docs[index].data()! as Map<String, dynamic>,
+                    );
+
+                    if(index==0){
+                      totalAmount = 0;
+                      totalAmount = totalAmount + (model.price! * seperateItemQuantityList![index]);
+                    }
+                    else
+                      {
+                        totalAmount = totalAmount + (model.price! * seperateItemQuantityList![index]);
+                      }
+
+                    if(snapshot.data!.docs.length - 1 == index){
+
+                      WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
+
+                        Provider.of<TotalAmount>(context, listen: false).displayTotalAmount(totalAmount.toDouble());
+
+                      });
+                    }
+
+                    return CartItemDesign(
+                      model: model,
+                      context: context,
+                      quanNumber: seperateItemQuantityList![index],
+                    );
+                  },
+                    childCount: snapshot.hasData ? snapshot.data!.docs.length : 0,
+                  ),
+
+                                 );
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/mainScreens/history_screen.dart
@@ -0,0 +1,60 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:hometodoor_user/assistantMethods/assistant_methods.dart';
+import 'package:hometodoor_user/global/global.dart';
+import 'package:hometodoor_user/widgets/order_card.dart';
+import 'package:hometodoor_user/widgets/progress_bar.dart';
+import 'package:hometodoor_user/widgets/simple_app_bar.dart';
+
+class HistoryScreen extends StatefulWidget {
+
+
+  @override
+  State<HistoryScreen> createState() => _HistoryScreenState();
+}
+
+class _HistoryScreenState extends State<HistoryScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+          appBar: SimpleAppBar(title: "History",),
+          body: StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance
+                .collection("users").doc(sharedPreferences!.getString("uid"))
+                .collection("orders").where("status", isEqualTo: "ended")
+                .orderBy("orderTime", descending: true).snapshots(),
+            builder: (c, snapshot){
+              return snapshot.hasData
+                  ? ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (c, index){
+                  return FutureBuilder<QuerySnapshot>(
+                    future: FirebaseFirestore.instance
+                        .collection("items")
+                        .where("itemID", whereIn: seperateOrderItemIDs((snapshot.data!.docs[index].data()! as Map<String, dynamic>) ["productIDs"]))
+                        .where("orderBy", whereIn: (snapshot.data!.docs[index].data()! as Map<String, dynamic>)["uid"])
+                        .orderBy("publishedDate", descending: true).get(),
+                    builder: (c, snap){
+                      return snap.hasData
+                          ? OrderCard(
+                        itemCount: snap.data!.docs.length,
+                        data: snap.data!.docs,
+                        orderID: snapshot.data!.docs[index].id,
+                        seperateQuantitiesList: seperateOrderItemQuantities((snapshot.data!.docs[index].data()! as Map<String, dynamic>) ["productIDs"]),
+                      )
+                          : Center(child: circularProgress(),);
+                    },
+                  );
+                },
+              )
+                  : Center(
+                child: circularProgress(),
+              );
+            },
+          ),
+        )
+    );
+  }
+}
